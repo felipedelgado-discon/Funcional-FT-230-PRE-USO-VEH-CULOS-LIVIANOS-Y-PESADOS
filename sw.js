fd1a9e09d5f255fde22222f5b60959d3e72fb2f2
felipedelgado-discon <felipe.delgado@discon.com.co>
2026-09-10 17:02:40 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,18 @@
-const CACHE_NAME = 'ft230-v2';
+const CACHE_NAME = 'ft230-v3';
+// Aquí definimos los archivos que el celular debe descargar
+const urlsToCache = [
+  './',
+  './index.html'
+];
 
 self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Guardando en caché el HTML');
+        return cache.addAll(urlsToCache);
+      })
+  );
   self.skipWaiting();
 });
 
@@ -14,11 +26,15 @@ self.addEventListener('activate', (e) => {
 });
 
 self.addEventListener('fetch', (e) => {
+  // Ignorar Apps Script
   if (e.request.url.includes('script.google.com')) {
     e.respondWith(fetch(e.request));
     return;
   }
+  // Servir el HTML desde la memoria caché
   e.respondWith(
-    fetch(e.request).catch(() => caches.match(e.request))
+    caches.match(e.request).then(response => {
+      return response || fetch(e.request);
+    }).catch(() => caches.match('./index.html'))
   );
 });
